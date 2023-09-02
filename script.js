0e890133a1c16dc0d0b712197cb3e89a8c4b4c13
Fufutis <fufutis15@gmail.com>
2023-09-02 17:39:29 +0300

    i got it working but barely

--- script.js
+++ script.js
@@ -9,7 +9,17 @@ function changeText() {
     document.querySelector("p").innerHTML = "You are now level " + (number+1) + " continue improving";
 }
 
+var width = 10; // Initial width
+
 function moveadd() {
+  var elem = document.getElementById("myBar");   
+  if (width < 100) {/* OK SO YOU GOT IT WORKING JUST LATER MAKE IT SO THE THING INSIDE OF THE BAR CHANGES AND NOT THE BAR IT  SELF*/
+    width += 10; // Increase by 10%
+    elem.style.width = width + '%'; 
+  }
+}
+
+/*function moveadd() {
   var elem = document.getElementById("myBar");
   var width = 1;
   var id = setInterval(frame, 10);
@@ -35,4 +45,4 @@ function movesub() {
       elem.style.width = width + '%';
     }
   }
-}
+}*/
