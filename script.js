677ccf9ae32149b80678cdad7d6c70f3a544bd0b
Fufutis <fufutis15@gmail.com>
2023-09-11 23:27:46 +0300

    instead of arrays we did objects ty nick

--- script.js
+++ script.js
@@ -8,53 +8,41 @@ function increment() {
 function changeText() {
     document.querySelector("p").innerHTML = "You are now level " + (number+1) + " continue improving";
 }
-var filled = 0; // Initial fill percentage
-
-function moveadd() {
-  var elem = document.getElementById("myBar");
-
-  if (filled <= 100) {
-    filled += 10; // Increase the fill by 10%
-
-    // Update the background fill using the data-filled attribute
-    elem.style.background = `linear-gradient(orange 0 0) 0/${filled}% no-repeat rgb(35, 35, 43)`;
-  }
-}
-function movesub() {
-  var elem = document.getElementById("myBar");
-
-  if (filled >= 1) {
-    filled -= 10; // Increase the fill by 10%
-
-    // Update the background fill using the data-filled attribute
-    elem.style.background = `linear-gradient(orange 0 0) 0/${filled}% no-repeat rgb(35, 35, 43)`;
-  }
+const INITIAL_FILL = 0; // Initial fill percentage
+let stats = {
+  strength: INITIAL_FILL,
+  dexterity: INITIAL_FILL,
+  intelligence: INITIAL_FILL,
+  perception: INITIAL_FILL,
+  endurance: INITIAL_FILL,
+  luck: INITIAL_FILL
 }
 
-/*function moveadd() {
-  var elem = document.getElementById("myBar");
-  var width = 1;
-  var id = setInterval(frame, 10);
-  function frame() {
-    if (width >= 100) {
-      clearInterval(id);
-    } else {
-      width++;
-      elem.style.width = width + '%';
-    }
-  }
+/**
+ * Updates the background of an element with a linear gradient based on the given stat percentage.
+ * @param {string} stat - The name of the stat to update.
+ */
+function updateBarForStat(stat) {
+  const elem = document.getElementById(stat);
+  elem.style.background = `linear-gradient(orange 0 0) 0/${stats[stat]}% no-repeat rgb(35, 35, 43)`;
 }
 
-function movesub() {
-  var elem = document.getElementById("myBar");
-  var width = 1;
-  var id = setInterval(frame, 10);
-  function frame() {
-    if (width >= 100) {
-      clearInterval(id);
-    } else {
-      width++;
-      elem.style.width = width + '%';
-    }
-  }
-}*/
+/**
+ * Increases the fill of a progress bar by 10% and updates its background fill.
+ * @param {string} stat - The stat to be increased.
+ */
+function moveadd(stat) {
+  // If the current stat + 10 exceeds our limit of 100, set it to 100
+  stats[stat] = Math.min(stats[stat] + 10, 100);
+  // Update the background fill using the data-filled attribute
+  updateBarForStat(stat);
+}
+
+/**
+ * Moves the sub-bar of a progress bar by increasing its fill by 10%.
+ * @param {string} stat - The stat to be decreased.
+ */
+function movesub(stat) {
+  stats[stat] = Math.max(stats[stat] - 10, 0);
+  updateBarForStat(stat);
+}
