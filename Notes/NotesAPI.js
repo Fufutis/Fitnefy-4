1c0879d5d8c264377aa705978ec22d3f36e8d9a0
Fufutis <fufutis15@gmail.com>
2023-09-27 08:02:24 +0300

    Completed-Notes and Adjusted-css

--- Notes/NotesAPI.js
+++ Notes/NotesAPI.js
@@ -1,36 +1,38 @@
 /*https://www.youtube.com/watch?v=01YKQmia2Jw&ab_channel=dcode*/
 // interacts with local storage to retrieve save and delete our notes
 export default class NotesAPI {
-  static getALLNotes() {
-    const notes = JSON.parse(localStorage.getItem("notesapp-notes") || "[]");
-    //if there are no existing notes in the system or give us an empty array (incomplete)
-    return notes.sort((a, b) => {
-      //sorts the array to latest based on the date
-      return new Date(a.updated) > new Date(b.updated) ? -1 : 1;
-    });
+  static getAllNotes() {
+      const notes = JSON.parse(localStorage.getItem("notesapp-notes") || "[]");
+
+      return notes.sort((a, b) => {
+          return new Date(a.updated) > new Date(b.updated) ? -1 : 1;
+      });
   }
+//-----------------------------------------------------------------------------
 
   static saveNote(noteToSave) {
-    const notes = NotesAPI.getALLNotes();
-    const existing = notes.find((note) => note.id == noteToSave.id);
+      const notes = NotesAPI.getAllNotes();
+      const existing = notes.find(note => note.id == noteToSave.id);
 
-    if (existing) {
-      existing.title = noteToSave.title;
-      existing.body = noteToSave.body;
-      existing.updated = new Date().toISOString();
-    } else {
-      noteToSave.id = Math.floor(Math.random() * 1000000); //creates an id for a note
-      noteToSave.updated = new Date().toISOString(); //timestamp for our note again (bcs we are creating one here so we are bringing it to existance)d
+      // Edit/Update
+      if (existing) {
+          existing.title = noteToSave.title;
+          existing.body = noteToSave.body;
+          existing.updated = new Date().toISOString();
+      } else {
+          noteToSave.id = Math.floor(Math.random() * 1000000);
+          noteToSave.updated = new Date().toISOString();
+          notes.push(noteToSave);
+      }
 
-      notes.push(noteToSave); //saves the note
-    }
-    localStorage.setItem("notesapp-notes", JSON.stringify(notes));
+      localStorage.setItem("notesapp-notes", JSON.stringify(notes));
   }
+//-----------------------------------------------------------------------------
 
   static deleteNote(id) {
-    const notes = NotesAPI.getALLNotes();
-    const newNotes = notes.filter((note) => note.id != id);
+      const notes = NotesAPI.getAllNotes();
+      const newNotes = notes.filter(note => note.id != id);
 
-    localStorage.setItem("notesapp-notes", JSON.stringify(newNotes));
+      localStorage.setItem("notesapp-notes", JSON.stringify(newNotes));
   }
 }
--- Notes/main.js
+++ Notes/main.js
@@ -1,31 +1,4 @@
-import NotesView from "./NotesView.js";
-import NotesAPI from "./NotesAPI.js";
+import App from "./App.js";
 
-const app = document.getElementById("app");
-const view = new NotesView(app, {
-  onNoteAdd() {
-    console.log("ADDED NEW NOTE");
-  },
-  onNoteEdit(newTitle, newBody) {
-    console.log(newTitle);
-    console.log(newBody);
-  },
-  onNoteSelect(id) {
-    console.log("Note Selected:" + id);
-  },
-  //onNoteAdd
-  //onNoteEdit
-  //onNoteDelete
-});
-/*import NotesAPI from "./NotesAPI.js";
-
-NotesAPI.saveNote({
-  id: 322909,
-  title: "The ti2tle has changed",
-  body: "WAGW2AN g",
-});
-*/
-//NotesAPI.deleteNote();
-//console.log(NotesAPI.getALLNotes());
-
-view.updatedNoteList(NotesAPI.getALLNotes());
+const root = document.getElementById("app");
+const app = new App(root);
